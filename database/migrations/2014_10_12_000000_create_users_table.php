33067bd885abb68043df11411f22307494a3f662
Jay0103l <jaylathiya1548@gmail.com>
2025-08-25 23:02:42 +0530

    add migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,12 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('phone_no');
+            $table->string('pan_number');
             $table->string('password');
+            $table->string('role');
+            $table->boolean('is_active')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
